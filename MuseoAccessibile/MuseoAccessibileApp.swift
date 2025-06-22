8935131a8a89e8e3b05bb133fc7e189f96bc76b7
davide <davide.merassi@studenti.unimi.it>
2025-06-22 16:11:44 +0200

    Initial Commit

---
+++ MuseoAccessibile/MuseoAccessibileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseoAccessibileApp.swift
+//  MuseoAccessibile
+//
+//  Created by Davide Merassi on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MuseoAccessibileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuseoAccessibileTests/MuseoAccessibileTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuseoAccessibileTests.swift
+//  MuseoAccessibileTests
+//
+//  Created by Davide Merassi on 22/06/25.
+//
+
+import Testing
+@testable import MuseoAccessibile
+
+struct MuseoAccessibileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
